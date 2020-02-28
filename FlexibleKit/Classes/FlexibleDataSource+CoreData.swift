f7bf186396689d11538c95c0e60938c0b0b71f05
Alexander Bozhko <alexander.bozhko@filmgrail.com>
2020-02-28 12:11:09 +0300

    fix for core data update before initialization

--- FlexibleKit/Classes/FlexibleDataSource+CoreData.swift
+++ FlexibleKit/Classes/FlexibleDataSource+CoreData.swift
@@ -12,10 +12,12 @@ public extension FlexibleDataSource {
     
     /// Prepares data source for data model changes.
     func willChangeContent() {
+        guard let classic = classic else { return }
         classic.snapshot.willChange()
     }
     
     func changeContent(_ object: ItemIdentifierType, at indexPath: IndexPath, for type: NSFetchedResultsChangeType) {
+        guard let classic = classic else { return }
         switch type {
         case .insert:
             classic.snapshot.insertItem(object, at: indexPath)
@@ -28,12 +30,14 @@ public extension FlexibleDataSource {
     }
     
     func didChangeContent(_ identifiers: [ItemIdentifierType], animated: Bool) {
+        guard let classic = classic else { return }
         classic.snapshot.items = identifiers
         classic.snapshot.update(collectionView)
         layout.update(collectionView, animated: animated, snapshot: classic.snapshot)
     }
     
     func reloadData(_ identifiers: [ItemIdentifierType]) {
+        guard let classic = classic else { return }
         classic.snapshot.reloadData(identifiers)
         classic.snapshot.update(collectionView)
         layout.update(collectionView, animated: false, snapshot: classic.snapshot)
